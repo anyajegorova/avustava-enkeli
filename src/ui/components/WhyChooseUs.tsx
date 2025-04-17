ea0669be0f0f268051f06e599adfcce595fe45d8
anyajegorova <anna.jerm@outlook.com>
2025-04-17 22:25:24 +0300

    reveal for why choose us page

--- src/ui/components/WhyChooseUs.tsx
+++ src/ui/components/WhyChooseUs.tsx
@@ -1,35 +1,49 @@
 import Title from './Title'
 import '../styles/WhyChooseUs.css'
 import { EnvironmentOutlined, ToolOutlined, SafetyOutlined, SyncOutlined } from '@ant-design/icons';
+import Reveal from '../../reveal_components/Reveal';
 
 const WhyChooseUs = () => {
   return (
     <div className="why-choose-us-container">
       <Title text="Miksi valita meidät?" />
+      
       <div className="section">
-        <EnvironmentOutlined className="icon" />
-        <h2>PAIKALLISSUUS</h2>
-        <hr />
-        <p>Olemme lähellä ja ymmärrämme sinun tarpeesi.</p>
+        <Reveal>
+          <EnvironmentOutlined className="icon" />
+          <h2>PAIKALLISSUUS</h2>
+          <hr />
+          <p>Olemme lähellä ja ymmärrämme sinun tarpeesi.</p>
+        </Reveal>
       </div>
+
       <div className="section">
-        <ToolOutlined className="icon" />
-        <h2>AMMATTITAITO</h2>
-        <hr />
-        <p>Voit luottaa meihin ja palvelumme korkeaan tasoon.</p>
+        <Reveal>
+          <ToolOutlined className="icon" />
+          <h2>AMMATTITAITO</h2>
+          <hr />
+          <p>Voit luottaa meihin ja palvelumme korkeaan tasoon.</p>
+        </Reveal>
       </div>
+
       <div className="section">
-        <SafetyOutlined className="icon" />
-        <h2>LUOTETTAVUUS</h2>
-        <hr />
-        <p>Työnjälkemme on huolellista ja laadukasta.</p>
+        <Reveal>
+          <SafetyOutlined className="icon" />
+          <h2>LUOTETTAVUUS</h2>
+          <hr />
+          <p>Työnjälkemme on huolellista ja laadukasta.</p>
+        </Reveal>
       </div>
+
       <div className="section">
-        <SyncOutlined className="icon" />
-        <h2>JOUSTAVUUS</h2>
-        <hr />
-        <p>Räätälöimme palvelut elämäntilanteesi mukaan.</p>
+        <Reveal>
+          <SyncOutlined className="icon" />
+          <h2>JOUSTAVUUS</h2>
+          <hr />
+          <p>Räätälöimme palvelut elämäntilanteesi mukaan.</p>
+        </Reveal>
       </div>
+
     </div>
   )
 }
