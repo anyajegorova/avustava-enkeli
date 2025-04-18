9f896db229e540a565097889eb7d40a8406d60f0
anyajegorova <anna.jerm@outlook.com>
2025-04-18 15:48:26 +0300

    updated styling for language switcher

--- src/ui/components/LanguageSwitcher.tsx
+++ src/ui/components/LanguageSwitcher.tsx
@@ -1,15 +1,17 @@
 import i18n from "../../utils/i18n";
-import '../styles/LanguageSwitcher.css'
+import '../styles/LanguageSwitcher.css';
 
 const LanguageSwitcher = () => {
-  const changeLanguage = (lng: string) => {
-    i18n.changeLanguage(lng);
+  const changeLanguage = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    i18n.changeLanguage(event.target.value);
   };
 
   return (
     <div className="language-switcher">
-      <button onClick={() => changeLanguage("en")}>English</button>
-      <button onClick={() => changeLanguage("fi")}>Finnish</button>
+      <select defaultValue="fi" onChange={changeLanguage}>
+        <option value="en">🇬🇧 English</option>
+        <option value="fi">🇫🇮 Finnish</option>
+      </select>
     </div>
   );
 };
